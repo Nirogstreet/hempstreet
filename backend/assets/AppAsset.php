1b8583961a475f6a9b8e53d8892dfc20dd943089
shiva <shiva@t9l.com>
2020-02-17 10:56:05 +0530

    blog section added

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,12 +12,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/site.css?v=0.2',
+        'css/jquery-ui.css',
+        'css/jquery.timepicker.css',
     ];
     public $js = [
+        //'js/jquery.geocomplete.min.js','position' => \yii\web\View::POS_HEAD,
+       ['js/jquery-1.11.0.js','position' => \yii\web\View::POS_HEAD],
+        'js/jquery-ui.js?v=0.1',
+        'js/backend.js?v=0.1',
+        'js/tag-it.js',
+         'js/jquery.timepicker.js',
+         'js/custom.js?v=5.2',
+         'js/app.js?v=0.1',
+        // 'js/bootstrap-select.js?v=0.1',
+       // 'js/croppi.js?v=0.3'
+       // 'js/jquery.geocomplete.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
 }
+
+
--- backend/config/main.php
+++ backend/config/main.php
@@ -1,9 +1,9 @@
 <?php
 $params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -11,13 +11,20 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+       'redactor' => [
+            'class' => 'yii\redactor\RedactorModule',
+//            'uploadDir' => '@frontend/web/images/blogs/',
+//            'uploadUrl' => 'https://nirogstreet.com/images/blogs',
+           ],
+    ],
+   
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => 'backend\models\BackendUser',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
@@ -34,17 +41,57 @@ return [
                 ],
             ],
         ],
+        'assetManager' => [
+        'appendTimestamp' => true,
+                'bundles' => [
+                    'yii\bootstrap\BootstrapAsset' => [
+                        'css' => [],
+                    ],
+                    'yii\web\JqueryAsset' => [
+                        'sourcePath' => null,
+                        'js' => []
+                    ]
+                ],
+        'bundles' => [
+        'dmstr\web\AdminLteAsset' => [
+            'skin' => 'skin-green-light',
+            ],
+        ],
+       ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
-        'urlManager' => [
+         'urlManager' =>  [
+            'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
+            //'suffix' => '.html',
             'rules' => [
+               'contact' => 'site/contact', 
+                
             ],
         ],
-        */
+        'urlManagerFrontend' => [
+                'class' => 'yii\web\urlManager',
+                'baseUrl' => 'http://hemp.loc',
+                'enablePrettyUrl' => true,
+                'showScriptName' => false,
+        ],
     ],
+    
+    'as beforeRequest' => [  //if guest user access site so, redirect to login page.
+    'class' => 'yii\filters\AccessControl',
+    'rules' => [
+        [
+            'actions' => ['login', 'error'],
+            'allow' => true,
+        ],
+        [
+            'allow' => true,
+            'roles' => ['@'],
+        ],
+    ],
+],
     'params' => $params,
 ];
