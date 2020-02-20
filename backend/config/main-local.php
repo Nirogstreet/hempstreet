292cd3f0317d72e71eae71df874063a93c7870f1
shiva <shiva@t9l.com>
2020-02-20 16:14:45 +0530

    index

---
+++ backend/config/main-local.php
@@ -0,0 +1,24 @@
+
+
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '3efy7ZPr7aZNrDlY4b0oV7auhfhLvc8h',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
+
